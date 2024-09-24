7dcbe44b272900716efc1d58138880e909c5ee0e
Felipe Espinoza <domingues.espinoza@outlook.com>
2024-09-24 18:33:53 -0300

    adicionando notas e calculo condicional

--- src/TarefaCalculoMedia.java
+++ src/TarefaCalculoMedia.java
@@ -4,4 +4,22 @@
  */
 
 public class TarefaCalculoMedia {
+    public static void main (String[] args){
+        calculoMedia();
+    }
+
+    private static void calculoMedia() {
+        int notaPortugues = 7;
+        int notaMatematica = 7;
+        int notaQuimica = 8;
+        int notaGeografia = 8;
+        float somaNotas = notaGeografia + notaMatematica + notaPortugues + notaQuimica;
+        float resultadoMedia = somaNotas/4;
+
+        if (resultadoMedia >= 7 && resultadoMedia <= 10){
+            System.out.println("Sua média é de " + resultadoMedia + ".\nVocê esta aprovado!");
+        } else {
+            System.out.println("A sua média foi de " + resultadoMedia + ".\nVocê esta reprovado!");
+        }
+    }
 }
